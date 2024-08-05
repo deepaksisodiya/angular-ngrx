002fd320d79f3d9644e3fd3027d8f674685028e8
Deepak Sisodiya <deepak.sisodiya@technogise.com>
2024-08-05 22:09:32 +0530

    initial code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { increment, decrement, reset } from './state/counter.actions';
+import { AppState } from './state/app.state';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-ngrx';
+
+  // constructor(private store: Store<AppState>) {}
+
+  // counter$ = this.store.select(state => state.counter);
+
+  // increment() {
+  //   this.store.dispatch(increment());
+  // }
+
+  // decrement() {
+  //   this.store.dispatch(decrement());
+  // }
+
+  // reset() {
+  //   this.store.dispatch(reset());
+  // }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { reducers } from './state/app.state'; 
+import { provideEffects } from '@ngrx/effects'
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideStore(reducers),
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+  ]
 };
